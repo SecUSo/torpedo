72f41ecc1ca84d77138f6a5bf3248729467fabc9
Betty Ballin <betty.ballin@yahoo.de>
2016-07-02 12:44:24 +0200

    konsolenausgabe entfernt

--- chrome/content/lib/handler.js
+++ chrome/content/lib/handler.js
@@ -123,9 +123,7 @@ torpedo.handler.mouseDownHref = function (event)
 torpedo.handler.mouseClickHref = function (event)
 {
 	var url = torpedo.functions.getHref();
-	Application.console.log("href clicked");
 	if(alreadyClicked == ""){
-		Application.console.log(alreadyClicked + " is alreadyClicked");
 		alreadyClicked = url;
 
 		var baseDomain = torpedo.functions.getDomainWithFFSuffix(url);
