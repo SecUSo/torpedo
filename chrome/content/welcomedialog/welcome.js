ab64c745a88521b0b0e8c4bc93924d0446602b2e
Betty Ballin <betty.ballin@yahoo.de>
2017-01-14 17:36:42 +0100

    edited tutorial pictures

--- chrome/content/welcomedialog/welcome.js
+++ chrome/content/welcomedialog/welcome.js
@@ -9,58 +9,58 @@ torpedo.welcome.picture = function(picture) {
 	}
 	if(picture==0){
 		if (navigator.language.indexOf("de") > -1)
-			document.getElementById("welcomePic").src="chrome://torpedo/skin/tutorial1_de.png";
-		else document.getElementById("welcomePic").src="chrome://torpedo/skin/tutorial1_en.png";
+			document.getElementById("welcomePic").src="chrome://torpedo/skin/tutorial_de-01.png";
+		else document.getElementById("welcomePic").src="chrome://torpedo/skin/tutorial_en-01.png";
 	}
 	if(picture==1){
 		if (navigator.language.indexOf("de") > -1)
-			document.getElementById("welcomePic1").src="chrome://torpedo/skin/tutorial2_de.png";
-		else document.getElementById("welcomePic1").src="chrome://torpedo/skin/tutorial2_en.png";
+			document.getElementById("welcomePic1").src="chrome://torpedo/skin/tutorial_de-02.png";
+		else document.getElementById("welcomePic1").src="chrome://torpedo/skin/tutorial_en-02.png";
 	}
 	if(picture==2){
 		if (navigator.language.indexOf("de") > -1)
-			document.getElementById("welcomePic2").src="chrome://torpedo/skin/tutorial3_de.png";
-		else document.getElementById("welcomePic2").src="chrome://torpedo/skin/tutorial3_en.png";
+			document.getElementById("welcomePic2").src="chrome://torpedo/skin/tutorial_de-03.png";
+		else document.getElementById("welcomePic2").src="chrome://torpedo/skin/tutorial_en-03.png";
 	}
 	if(picture==3){
 		if (navigator.language.indexOf("de") > -1)
-			document.getElementById("welcomePic3").src="chrome://torpedo/skin/tutorial4_de.png";
-		else document.getElementById("welcomePic3").src="chrome://torpedo/skin/tutorial4_en.png";
+			document.getElementById("welcomePic3").src="chrome://torpedo/skin/tutorial_de-04.png";
+		else document.getElementById("welcomePic3").src="chrome://torpedo/skin/tutorial_en-04.png";
 	}
 	if(picture==4){
 		if (navigator.language.indexOf("de") > -1)
-			document.getElementById("welcomePic4").src="chrome://torpedo/skin/tutorial5_de.png";
-		else document.getElementById("welcomePic4").src="chrome://torpedo/skin/tutorial5_en.png";
+			document.getElementById("welcomePic4").src="chrome://torpedo/skin/tutorial_de-05.png";
+		else document.getElementById("welcomePic4").src="chrome://torpedo/skin/tutorial_en-05.png";
 	}
 	if(picture==5){
 		if (navigator.language.indexOf("de") > -1)
-			document.getElementById("welcomePic5").src="chrome://torpedo/skin/tutorial6_de.png";
-		else document.getElementById("welcomePic5").src="chrome://torpedo/skin/tutorial6_en.png";
+			document.getElementById("welcomePic5").src="chrome://torpedo/skin/tutorial_de-06.png";
+		else document.getElementById("welcomePic5").src="chrome://torpedo/skin/tutorial_en-06.png";
 	}
 	if(picture==6){
 		if (navigator.language.indexOf("de") > -1)
-			document.getElementById("welcomePic6").src="chrome://torpedo/skin/tutorial7_de.png";
-		else document.getElementById("welcomePic6").src="chrome://torpedo/skin/tutorial7_en.png";
+			document.getElementById("welcomePic6").src="chrome://torpedo/skin/tutorial_de-07.png";
+		else document.getElementById("welcomePic6").src="chrome://torpedo/skin/tutorial_en-07.png";
 	}
 	if(picture==7){
 		if (navigator.language.indexOf("de") > -1)
-			document.getElementById("welcomePic7").src="chrome://torpedo/skin/tutorial8_de.png";
-		else document.getElementById("welcomePic7").src="chrome://torpedo/skin/tutorial8_en.png";
+			document.getElementById("welcomePic7").src="chrome://torpedo/skin/tutorial_de-08.png";
+		else document.getElementById("welcomePic7").src="chrome://torpedo/skin/tutorial_en-08.png";
 	}
 	if(picture==8){
 		if (navigator.language.indexOf("de") > -1)
-			document.getElementById("welcomePic8").src="chrome://torpedo/skin/tutorial9_de.png";
-		else document.getElementById("welcomePic8").src="chrome://torpedo/skin/tutorial9_en.png";
+			document.getElementById("welcomePic8").src="chrome://torpedo/skin/tutorial_de-09.png";
+		else document.getElementById("welcomePic8").src="chrome://torpedo/skin/tutorial_en-09.png";
 	}
 	if(picture==9){
 		if (navigator.language.indexOf("de") > -1)
-			document.getElementById("welcomePic9").src="chrome://torpedo/skin/tutorial10_de.png";
-		else document.getElementById("welcomePic9").src="chrome://torpedo/skin/tutorial10_en.png";
+			document.getElementById("welcomePic9").src="chrome://torpedo/skin/tutorial_de-10.png";
+		else document.getElementById("welcomePic9").src="chrome://torpedo/skin/tutorial_en-10.png";
 	}
 	if(picture==10){
 		if (navigator.language.indexOf("de") > -1)
-			document.getElementById("welcomePic10").src="chrome://torpedo/skin/tutorial11_de.png";
-		else document.getElementById("welcomePic10").src="chrome://torpedo/skin/tutorial11_en.png";
+			document.getElementById("welcomePic10").src="chrome://torpedo/skin/tutorial_de-11.png";
+		else document.getElementById("welcomePic10").src="chrome://torpedo/skin/tutorial_en-11.png";
 	}
 }
 
